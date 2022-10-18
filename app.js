37a15bc88e8ccdba248224933356be017aee5f5b
mac <dadebayo200@gmail.com>
2022-10-18 19:01:44 +0100

    login and sign up/ routes done

---
+++ app.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+
+app.get('/', (req, res) => {
+    res.redirect('/login');
+});
+
+
+app.get('/login', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'signup.html'));
+});
+
+app.get('/board', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
+});
+
+app.listen(5000, 
+console.log(`server is listening`)
+); 
